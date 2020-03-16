4d162d82293547f9fcd689ccfefc1dab0c4b1bb1
Rilton Franzone <riltonfranzone@gmail.com>
2020-03-15 22:27:00 -0300

    feat: user profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { utcToZonedTime, format } from 'date-fns-tz';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+
+import {
+  Container,
+  UserInfo,
+  UserImage,
+  UserField,
+  Title,
+  UserData,
+  LogoutButton,
+  ButtonText,
+} from './styles';
+
+const pattern = 'dd/MM/yyyy';
+const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
 
 export default function Profile() {
+  const user = useSelector(state => state.auth.user);
+
+  const zonedDate = utcToZonedTime(user.created_at, timeZone);
+  const formattedDate = format(zonedDate, pattern);
+
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
   return (
     <Container>
-      <Text>Profile page</Text>
+      <UserInfo>
+        <UserImage
+          source={{
+            uri:
+              (user.avatar && user.avatar.url) ||
+              `https://ui-avatars.com/api/?size=140&background=f4effc&color=a28fd0&name=${user.name.replace(
+                /\s/g,
+                '+'
+              )}`,
+          }}
+        />
+        <UserField>
+          <Title>Nome Completo</Title>
+          <UserData>{user.name}</UserData>
+        </UserField>
+        <UserField>
+          <Title>Email</Title>
+          <UserData>{user.email}</UserData>
+        </UserField>
+        <UserField>
+          <Title>Data de cadastro</Title>
+          <UserData>{formattedDate}</UserData>
+        </UserField>
+        <LogoutButton onPress={handleLogout}>
+          <ButtonText>Logout</ButtonText>
+        </LogoutButton>
+      </UserInfo>
     </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,8 +1,53 @@
 import styled from 'styled-components/native';
 
+import { RectButton } from 'react-native-gesture-handler';
+
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: #ffffff;
+`;
+
+export const UserInfo = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const UserImage = styled.Image`
+  width: 140px;
+  height: 140px;
+  border-radius: 70px;
+  margin-bottom: 30px;
+`;
+export const UserField = styled.View`
+  width: 305px;
+  margin-top: 15px;
+`;
+
+export const Title = styled.Text`
+  color: #666666;
+  font-size: 12px;
+`;
+
+export const UserData = styled.Text`
+  color: #444444;
+  font-weight: bold;
+  font-size: 22px;
+`;
+
+export const LogoutButton = styled(RectButton)`
+  margin-top: 30px;
+  background-color: #e74040;
+  width: 305px;
+  height: 40px;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #ffff;
 `;
