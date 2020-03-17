c2a3db95b40510918285acb100427aeac35926f6
Rilton Franzone <riltonfranzone@gmail.com>
2020-03-17 18:22:33 -0300

    feat: list delivery problems

--- src/pages/ViewProblem/index.js
+++ src/pages/ViewProblem/index.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { utcToZonedTime, format } from 'date-fns-tz';
+import PropTypes from 'prop-types';
+import { Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Container, TopBox } from './styles';
+import {
+  Container,
+  TopBox,
+  Title,
+  ProblemsList,
+  CardContainer,
+  ProblemDescription,
+  ProblemDate,
+  EmptyMessage,
+  EmptyText,
+} from './styles';
 
-export default function ViewProblem() {
+import api from '~/services/api';
+
+const pattern = 'dd/MM/yyyy';
+const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
+
+function Card({ problem }) {
+  const zonedDate = utcToZonedTime(problem.createdAt, timeZone);
+  const problemDate = format(zonedDate, pattern);
+
+  return (
+    <CardContainer>
+      <ProblemDescription>{problem.description}</ProblemDescription>
+      <ProblemDate>{problemDate}</ProblemDate>
+    </CardContainer>
+  );
+}
+
+export default function ViewProblem({ route }) {
+  const { id } = route.params;
+  const [problems, setProblems] = useState([]);
+
+  async function loadData() {
+    try {
+      const response = await api.get(`delivery/${id}/problems`);
+      setProblems(response.data);
+    } catch (err) {
+      Alert.alert('Houve um erro no servidor');
+    }
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <Container>
       <TopBox />
+      <Title>{`Encomenda ${id}`}</Title>
+      {problems.length >= 1 ? (
+        <ProblemsList
+          data={problems}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Card problem={item} />}
+        />
+      ) : (
+        <EmptyMessage>
+          <EmptyText>Nenhum problema registrado na encomenda</EmptyText>
+          <Icon name="check-circle-outline" size={50} color="#7D40E7" />
+        </EmptyMessage>
+      )}
     </Container>
   );
 }
+
+ViewProblem.propTypes = {
+  route: PropTypes.object.isRequired,
+};
+
+Card.propTypes = {
+  problem: PropTypes.object.isRequired,
+};
--- src/pages/ViewProblem/styles.js
+++ src/pages/ViewProblem/styles.js
@@ -11,3 +11,53 @@ export const TopBox = styled.View`
   background-color: #7d40e7;
   align-items: center;
 `;
+
+export const Title = styled.Text`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 18px;
+  margin: -60px 0 10px 0;
+  text-align: center;
+`;
+
+export const ProblemsList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const CardContainer = styled.View`
+  width: 85%;
+  height: 55px;
+  border-radius: 4px;
+  background-color: #ffffff;
+  border: 1px solid #0000001a;
+  align-self: center;
+  margin-bottom: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+`;
+
+export const ProblemDescription = styled.Text`
+  color: #444444;
+  font-size: 16px;
+`;
+
+export const ProblemDate = styled.Text`
+  color: #999999;
+  font-size: 12px;
+`;
+
+export const EmptyMessage = styled.View`
+  margin: auto;
+  width: 85%;
+  align-items: center;
+`;
+
+export const EmptyText = styled.Text`
+  color: #444444;
+  text-align: center;
+  margin-bottom: 30px;
+  font-weight: bold;
+  font-size: 25px;
+`;
